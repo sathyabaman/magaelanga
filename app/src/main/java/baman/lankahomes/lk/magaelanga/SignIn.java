66842890e79e6896520b7672e807e9f1ddf131d9
sathyabaman <sathyabaman@isaithuli.com>
2016-06-05 13:45:52 +0530

    Signup design done

---
+++ app/src/main/java/baman/lankahomes/lk/magaelanga/SignIn.java
@@ -0,0 +1,27 @@
+package baman.lankahomes.lk.magaelanga;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SignIn extends ActionBarActivity {
+
+    Button btn_signin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+    }
+
+    public void sign_in(View v){
+        startActivity(new Intent(SignIn.this, MainActivity.class));
+    }
+
+    public void sign_up(View v){
+        startActivity(new Intent(SignIn.this, SignUp.class));
+    }
+}
--- app/src/main/java/baman/lankahomes/lk/magaelanga/SignUp.java
+++ app/src/main/java/baman/lankahomes/lk/magaelanga/SignUp.java
@@ -1,23 +1,13 @@
 package baman.lankahomes.lk.magaelanga;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.widget.Button;
+import android.os.Bundle;
 
 public class SignUp extends ActionBarActivity {
 
-    Button btn_signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-
-    }
-
-    public void sign_in(View v){
-        startActivity(new Intent(SignUp.this, MainActivity.class));
     }
 }
